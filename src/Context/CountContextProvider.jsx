506c4b22645bb77fbfa450f32494d55d0c827465
Tapishagrawal <tapishg4@gmail.com>
2023-10-14 00:58:42 +0530

    done with null

--- src/Context/CountContextProvider.jsx
+++ src/Context/CountContextProvider.jsx
@@ -4,8 +4,20 @@ import { getDataFromLocal } from "../redux/localReducer/action";
 export const CountContext = createContext();
 
 export default function CountContextProvider({ children }) {
-    const [cartCount, setCartCount] = useState(getDataFromLocal("cartData").length || null)
-    const [wishListCount, setWishListCount] = useState(getDataFromLocal("wishListData").length || null)
+    const [cartCount, setCartCount] = useState(0); // Initialize with 0
+    const [wishListCount, setWishListCount] = useState(0); // Initialize with 0
+
+    useEffect(() => {
+        const cartData = getDataFromLocal("cartData");
+        if (cartData) {
+            setCartCount(cartData.length);
+        }
+
+        const wishListData = getDataFromLocal("wishListData");
+        if (wishListData) {
+            setWishListCount(wishListData.length);
+        }
+    }, []);
     return (
         <CountContext.Provider value={{ cartCount, setCartCount, wishListCount, setWishListCount }}>
             <div>
