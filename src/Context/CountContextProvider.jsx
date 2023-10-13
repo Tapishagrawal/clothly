87a3dae51000da890bd2f55fa9a167406c835d09
Tapishagrawal <tapishg4@gmail.com>
2023-10-14 00:53:25 +0530

    adding null condtion

--- src/Context/CountContextProvider.jsx
+++ src/Context/CountContextProvider.jsx
@@ -4,8 +4,8 @@ import { getDataFromLocal } from "../redux/localReducer/action";
 export const CountContext = createContext();
 
 export default function CountContextProvider({ children }) {
-    const [cartCount, setCartCount] = useState(getDataFromLocal("cartData").length || 0)
-    const [wishListCount, setWishListCount] = useState(getDataFromLocal("wishListData").length || 0)
+    const [cartCount, setCartCount] = useState(getDataFromLocal("cartData").length || null)
+    const [wishListCount, setWishListCount] = useState(getDataFromLocal("wishListData").length || null)
     return (
         <CountContext.Provider value={{ cartCount, setCartCount, wishListCount, setWishListCount }}>
             <div>
